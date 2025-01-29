c7c5a6144767a77277fedb58e0587c235907188a
Ravi Patil <captainkohli@Ravis-MacBook-Air.local>
2025-01-29 16:21:51 +0530

    fixing relative path prob for production

--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -1,5 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
+//import burgerImage from './right side/burger.png';
+//import pngwing2 from '/right side/pngwing.com (12).png';
+
 import "./Header.css"
+const burgerImage ='/right side/burger.png';
+const pngwing2 ='/right side/buger2.png';
+const pngwing3 ='/right side/buger2.png';
+const pngwing4 ='/right side/Flying Burger PNG Image - 2084x2084.png.png';
+const pngwing5 ='/right side/pngwing.com (16).png'
 export default function Header() {
     //let toggle_image_address=;
     const [toggle_image_address,setToggle_image_address]=useState('../public/right side/Flying Burger PNG Image - 2084x2084.png')
@@ -11,28 +20,67 @@ export default function Header() {
   return (
     <header>
       <div className="text">
-        <h1>THE ULTIMATE <br/>BURGER SHOP</h1>
+        <h1>
+          THE ULTIMATE <br />
+          PATIL'S CAFE
+        </h1>
         <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta,
-        corporis. Architecto at animi ratione officiis saepe,cumque explicabo
-        nihil nulla corporis, eveniet, quas ea voluptatem laboriosam atque
-        maiores. Dicta, beatae.
+          Crispy fries, golden brown and hot,<br/>
+           A perfect pairing, can't be forgot.<br/>
+            With coffee's warmth, a simple treat,<br/>
+           A tasty moment,oh so sweet.
         </p>
-        <div className='buttons-section'>
-        <a href='' className='btn'>Order Now</a>
-        <a href='' className='btn'>View More</a>
+        <div className="buttons-section">
+          <a href="" className="btn">
+            Order Now
+          </a>
+          <a href="" className="btn">
+            View More
+          </a>
         </div>
       </div>
-      <div className='burger'>
-        <img src='../public/right side/burger.png' alt="burger iamage" onClick={(e)=>toggleBurger(e,'../public/right side/burger.png')}></img>
-        <img src='../public/right side/pngwing.com (12).png' alt="pngwing.com (12)" onClick={(e)=>toggleBurger(e,'../public/right side/pngwing.com (12).png')} ></img>
-        <img src='../public/right side/buger2.png' alt="burger2 iamage" onClick={(e)=>toggleBurger(e,'../public/right side/buger2.png')}></img>
-        <img src='../public/right side/Flying Burger PNG Image - 2084x2084.png' alt="Flying Burger PNG Image - 2084x2084" onClick={(e)=>toggleBurger(e,'../public/right side/Flying Burger PNG Image - 2084x2084.png')}></img>
-        <img src='../public/right side/pngwing.com (16).png' alt="pngwing.com (16)" onClick={(e)=>toggleBurger(e,'../public/right side/pngwing.com (16).png')}></img>
-        
+      <div className="burger">
+        <img
+          src={burgerImage}
+          alt="burger iamage"
+          onClick={(e) => toggleBurger(e, burgerImage)}
+        ></img>
+        <img
+          src={pngwing2}
+          alt="pngwing.com (12)"
+          onClick={(e) =>
+            toggleBurger(e, pngwing2)
+          }
+        ></img>
+        <img
+          src={pngwing3}
+          alt="burger2 iamage"
+          onClick={(e) => toggleBurger(e, pngwing3)}
+        ></img>
+        <img
+          src={pngwing4}
+          alt="Flying Burger PNG Image - 2084x2084"
+          onClick={(e) =>
+            toggleBurger(
+              e,
+              pngwing4
+            )
+          }
+        ></img>
+        <img
+          src={pngwing5}
+          alt="pngwing.com (16)"
+          onClick={(e) =>
+            toggleBurger(e, pngwing5)
+          }
+        ></img>
       </div>
-      <div className='burger-toggle'>
-        <img src={toggle_image_address} alt={toggle_image_address} className="active"></img>
+      <div className="burger-toggle">
+        <img
+          src={toggle_image_address}
+          alt={toggle_image_address}
+          className="active"
+        ></img>
       </div>
     </header>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { default as Homepage1 } from '../features/Homepage';
 export default function Homepage() {
   return (
-    <div>
-      homepage 
+    <div> 
       <Homepage1></Homepage1>
     </div>
   )
